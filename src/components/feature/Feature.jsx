575fc1d35ff0a11306a8df3c53c898bcc662e38c
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-10-10 14:55:00 -0700

    feat: create brand and whatgpt3 components

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import './feature.css';
 
 
-const Feature = () => {
+const Feature = ({title,
+text}) => {
   return (
-    <div>Feature</div>
+    <div className='gpt3__features-container_feature'>
+      <div className="gpt3__features-container_feature-title">
+        <h1>{ title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        { text}
+      </div>
+    </div>
+
   )
 }
 
