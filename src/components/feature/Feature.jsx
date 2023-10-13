57d46c4f689cba31457c1bf8d7f5df753c12e905
AVViktoria <56619435+AVViktoria@users.noreply.github.com>
2023-10-12 21:18:16 -0700

    feat: create feature component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import './feature.css';
+import React from "react";
+import "./feature.css";
 
-
-const Feature = ({title,
-text}) => {
+const Feature = ({ title, text }) => {
   return (
-    <div className='gpt3__features-container_feature'>
+    <div className="gpt3__features-container_feature">
       <div className="gpt3__features-container_feature-title">
-        <h1>{ title}</h1>
-      </div>
+        <div/>
+          <h1>{title}</h1>
+        </div>
       <div className="gpt3__features-container_feature-text">
-        { text}
+        <p> {text}</p>
       </div>
     </div>
-
-  )
-}
+  );
+};
 
 export default Feature;
